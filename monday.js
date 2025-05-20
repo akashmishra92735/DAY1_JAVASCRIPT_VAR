9f83b1aa807b88ec3e756230397db19daab71654
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-20 11:16:56 +0530

    Add files via upload

--- monday.js
+++ monday.js
@@ -1,20 +1,18 @@
-/*console.log("hello world");
-console.log("hii");
-console.log(12);
-*/
-// keyword let var const
+console.log("hello world");//"Print 'Hello World' to the console."
+//It is a JavaScript command used to display the message "Hello World" in the web browser's console.
+// KEYWORD
+//(1)var,(2)let,(3)Console
+//(1)var
+var num=80;//"Declare a variable named num and assign it the value 80."
+var num=20;//In this we can redeclare the variable.
+console.log(num);//Print the value of the variable num to the console.
 
-var num=20;
-console.log(num);
-var num=85;
+//(2)let
+let a=50;//Declare a block-scoped variable named a and assign it the value 50.
+let a=40;//In this we can not redeclare the variable.
+console.log(a);////Print the value of the variable a to the console.
 
-console.log(num);
-let a=50;
-let a=40;
-
-console.log(a);
-
-const b=12;
-const b=20;
-
-console.log(b);
+//(3)console
+const b=12;//Declare a constant variable named b and assign it the value 12.
+const b=20;//In this we can not redeclare the variable.
+console.log(a);//Print the value of the variable a to the console.
