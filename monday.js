8f13b1090e50ed3bebb3b828a096d991771dbd1c
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-20 11:14:56 +0530

    Add files via upload

---
+++ monday.js
@@ -0,0 +1,20 @@
+/*console.log("hello world");
+console.log("hii");
+console.log(12);
+*/
+// keyword let var const
+
+var num=20;
+console.log(num);
+var num=85;
+
+console.log(num);
+let a=50;
+let a=40;
+
+console.log(a);
+
+const b=12;
+const b=20;
+
+console.log(b);
